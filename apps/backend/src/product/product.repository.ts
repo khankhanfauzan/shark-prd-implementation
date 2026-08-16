5f1cd780eea2a0bae014f137f82208dfe7ef1368
khankhanfauzan <khankhanfauzan@gmail.com>
2026-08-16 21:28:07 +0800

    refactor: remove redundant getProducts logic in repository and service layers

--- apps/backend/src/product/product.repository.ts
+++ apps/backend/src/product/product.repository.ts
@@ -7,20 +7,6 @@ import { PrismaService } from "../../../../libs/database/prisma/prisma.service";
 export class ProductRepository {
   constructor(private prisma: PrismaService) { }
 
-  async getProducts() {
-    const result = await this.prisma.product.findMany({
-      include: {
-        reviews: {
-          select: {
-            rating: true
-          }
-        }
-      }
-    });
-
-    return result;
-  }
-
   // ambil produk + agregat rating (AVG & COUNT)
   async getProductWithSummary() {
     const product = await this.prisma.product.findFirst();
--- apps/backend/src/product/product.service.ts
+++ apps/backend/src/product/product.service.ts
@@ -7,27 +7,9 @@ import { roundToDecimal } from '../commons/utils/math.utiil';
 export class ProductService {
   constructor(private readonly productRepository: ProductRepository) { }
 
-  async getProductsRatingSummary(): Promise<RatingSummaryResponse[]> {
-    const products = await this.productRepository.getProducts();
-
-    return products.map(product => {
-      const totalReviews = product.reviews.length;
-      const rawAverage = totalReviews > 0
-        ? product.reviews.reduce((sum, review) => sum + review.rating, 0) / totalReviews
-        : 0;
-      const averageRating = Number(Math.round(Number(rawAverage + 'e1')) + 'e-1');
-
-      return {
-        productId: product.id,
-        averageRating,
-        totalReviews
-      };
-    });
-  }
-
-  // ambil product + agregat rating (AVG & COUNT)
   async getRatingSummary(): Promise<RatingSummaryResponse> {
     const data = await this.productRepository.getProductWithSummary();
+
     if (!data) {
       throw new NotFoundException('Product not found');
     }
